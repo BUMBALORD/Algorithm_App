fad7cad717921e4b750037b90ca873559254e604
DAVID HALLINAN <davidhallinan@gmail.com>
2016-05-12 08:56:30 -0700

    refactor tests

--- spec/algo_spec.rb
+++ spec/algo_spec.rb
@@ -5,16 +5,13 @@ RSpec.describe Algo, "#algo" do
   context "from creation of new algorithm" do
 
     it "should have a name and an algorithm" do
-      algo = Algo.new
-      algo.name = "fizzyBuzz"
-      algo.algorithm = "Make a soda go boom"
+      algo = Algo.new(:name => "fizzyBuzz", :algorithm => "Make a soda go boom")
       expect(algo.name).to eq "fizzyBuzz"
       expect(algo.algorithm).to eq "Make a soda go boom"
     end
 
     it "should be able to be set as a favorite" do
-      algo = Algo.new
-      algo.fav = true
+      algo = Algo.new(:fav => true)
       expect(algo.fav).to eq true
     end
 
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,11 +4,8 @@ RSpec.describe Comment, type: :model do
   context "from creation of new comment"
 
     it "should have a name and a comment" do
-      comment = Comment.new
-      comment.commenter = "Johnathan"
-      comment.body = "Sweet test, bro!"
+      comment = Comment.new(:commenter => "Johnathan", :body => "Sweet test, bro!")
       expect(comment.commenter).to eq "Johnathan"
       expect(comment.body).to eq "Sweet test, bro!"
     end
-
 end
