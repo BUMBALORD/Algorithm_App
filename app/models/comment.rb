daf81cbfead154e026b3ff682473511a26c75b72
DAVID HALLINAN <davidhallinan@gmail.com>
2016-05-11 18:49:28 -0700

    add a profanity filter for comments cuz why not

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,13 @@ class Comment < ActiveRecord::Base
   validates_presence_of :commenter
   validates_presence_of :body
 
+  def sanitized
+    comment = self.body
+    words = {'fuck' => 'frick', 'shit'=>'poop', 'bitch'=>'big ol meanie', 'Fuck' => 'Frick', 'Shit'=>'Poop', 'Bitch'=>'Biznatch'}
+    words.each do |swear, clean|
+      comment.gsub!(swear, clean)
+    end
+    return comment
+  end
+
 end
